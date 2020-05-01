ca4c0e4b2486c0df4a9d5654485bc3e561daa45f
tanyuehong <957963898@qq.com>
2020-05-01 16:08:20 +0800

    配置sql性能测试工具

--- service/service_edu/src/main/java/com/redskt/collegeservice/config/CollegeConfig.java
+++ service/service_edu/src/main/java/com/redskt/collegeservice/config/CollegeConfig.java
@@ -1,9 +1,26 @@
 package com.redskt.collegeservice.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @MapperScan("com.redskt.collegeservice.mapper")
 public class CollegeConfig {
+
+/**
+ * SQL 执行性能分析插件
+ * 开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长
+ */
+    @Bean
+    @Profile({"dev","test"})// 设置 dev test 环境开启
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setMaxTime(500);//ms，超过此处设置的ms则sql不执行
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
+    }
+
 }
