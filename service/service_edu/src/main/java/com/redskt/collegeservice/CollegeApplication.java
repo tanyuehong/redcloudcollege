288d4801638b26a52725f49fb88a1bc695147e6b
tanyuehong <957963898@qq.com>
2020-05-01 16:43:49 +0800

    api接口的开发

--- service/service_edu/src/main/java/com/redskt/collegeservice/CollegeApplication.java
+++ service/service_edu/src/main/java/com/redskt/collegeservice/CollegeApplication.java
@@ -2,8 +2,10 @@ package com.redskt.collegeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.redskt"})
 public class CollegeApplication {
 
     public static void main(String[] args) {
