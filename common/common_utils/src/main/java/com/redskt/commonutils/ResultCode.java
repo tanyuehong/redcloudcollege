32b8abb9c8013486ef776169c065640c8cb95d55
tanyuehong <957963898@qq.com>
2020-05-01 17:34:27 +0800

    统一接口返回

---
+++ common/common_utils/src/main/java/com/redskt/commonutils/ResultCode.java
@@ -0,0 +1,8 @@
+package com.redskt.commonutils;
+
+public class ResultCode {
+
+    public static Integer SUCCESS = 20000;
+    public static Integer ERROR = 20001;
+
+}
--- service/service_edu/src/main/java/com/redskt/collegeservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/redskt/collegeservice/controller/EduTeacherController.java
@@ -1,8 +1,11 @@
 package com.redskt.collegeservice.controller;
 
 
+import com.redskt.commonutils.R;
 import com.redskt.collegeservice.entity.EduTeacher;
 import com.redskt.collegeservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,7 @@ import java.util.List;
  * @author tanyuehong
  * @since 2020-04-26
  */
+@Api(description="讲师管理")
 @RestController
 @RequestMapping("/collegeservice/edu-teacher")
 public class EduTeacherController {
@@ -26,10 +30,11 @@ public class EduTeacherController {
     @Autowired
     private EduTeacherService teacherService;
 
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher() {
+    public R findAllTeacher() {
         List<EduTeacher> list = teacherService.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
 }
 
