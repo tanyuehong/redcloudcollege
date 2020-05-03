0f084fa997d5ae759e100f58a71c73e4263f0676
tanyuehong <957963898@qq.com>
2020-05-03 10:29:44 +0800

    代码提交

--- service/service_edu/src/main/java/com/redskt/collegeservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/redskt/collegeservice/controller/EduTeacherController.java
@@ -1,16 +1,18 @@
 package com.redskt.collegeservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.redskt.collegeservice.entity.query.TeacherQuery;
 import com.redskt.commonutils.R;
 import com.redskt.collegeservice.entity.EduTeacher;
 import com.redskt.collegeservice.service.EduTeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +24,11 @@ import java.util.List;
  * @author tanyuehong
  * @since 2020-04-26
  */
+@Slf4j
 @Api(description="讲师管理")
 @RestController
 @RequestMapping("/collegeservice/edu-teacher")
+@CrossOrigin(allowCredentials="true",maxAge = 3600)
 public class EduTeacherController {
     // 把service注入
     @Autowired
@@ -35,6 +39,48 @@ public class EduTeacherController {
     public R findAllTeacher() {
         List<EduTeacher> list = teacherService.list(null);
         return R.ok().data("items",list);
+    }
+
+    //4 条件查询带分页的方法
+    @PostMapping("pageTeacherCondition")
+    public R pageTeacherCondition(@RequestBody  TeacherQuery teacherQuery) {
+        //创建page对象
+        Integer current = teacherQuery.getCurrent();
+        Integer limit = teacherQuery.getLimit();
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+
+        //构建条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        // 多条件组合查询
+        // mybatis学过 动态sql
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //判断条件值是否为空，如果不为空拼接条件
+        if(!StringUtils.isEmpty(name)) {
+            //构建条件
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create",end);
+        }
+
+        //排序
+        wrapper.orderByDesc("gmt_create");
+
+        //调用方法实现条件查询分页
+        teacherService.page(pageTeacher,wrapper);
+
+        long total = pageTeacher.getTotal();//总记录数
+        List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
+        return R.ok().data("total",total).data("rows",records);
     }
 }
 
