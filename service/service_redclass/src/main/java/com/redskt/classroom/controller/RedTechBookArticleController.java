6a642406bf2f8052b58f3ec2f3bf574cf2cc4d90
tanyuehong <tanyuehong@qq.com>
2021-04-12 09:02:37 +0800

    书籍接口提交

--- service/service_redclass/src/main/java/com/redskt/classroom/controller/RedTechBookArticleController.java
+++ service/service_redclass/src/main/java/com/redskt/classroom/controller/RedTechBookArticleController.java
@@ -1,8 +1,11 @@
 package com.redskt.classroom.controller;
 
 
+import com.redskt.classroom.entity.EduBookContents;
 import com.redskt.classroom.entity.EduTechnologyBook;
 import com.redskt.classroom.entity.RedTechBookArticle;
+import com.redskt.classroom.service.EduBookContentsService;
+import com.redskt.classroom.service.EduTechnologyBookService;
 import com.redskt.classroom.service.RedTechBookArticleService;
 import com.redskt.commonutils.R;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,17 +24,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-02-28
  */
 @RestController
-@RequestMapping("/home/book/article")
+@RequestMapping("/home/book")
 public class RedTechBookArticleController {
 
     @Autowired
     private RedTechBookArticleService articleService;
 
-    @GetMapping("getBookArticleDetail/{id}")
+    @Autowired
+    private EduTechnologyBookService bookService;
+
+    @Autowired
+    private EduBookContentsService contentsService;
+
+    @GetMapping("getBookArticleDetail/{articleId}")
     public R getBookDetail(@PathVariable String articleId) {
         if(articleId.length()>0) {
             RedTechBookArticle article = articleService.getById(articleId);
-            return  R.ok().data("article",article);
+            EduTechnologyBook book = bookService.getById(article.getBookId());
+            EduBookContents contents = contentsService.getById(article.getContentId());
+            return  R.ok().data("item",article).data("book",book).data("contents",contents);
         } else {
             return R.error("哦哦,参数错误哈");
         }
--- service/service_redclass/src/main/java/com/redskt/classroom/entity/EduBookContents.java
+++ service/service_redclass/src/main/java/com/redskt/classroom/entity/EduBookContents.java
@@ -38,6 +38,8 @@ public class EduBookContents implements Serializable {
 
     private String chapterId;
 
+    private String articleId;
+
     private String title;
 
     private Integer sortcontent;
