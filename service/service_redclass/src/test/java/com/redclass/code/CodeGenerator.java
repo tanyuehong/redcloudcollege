2ffb484adf7c8d405b8f295d08aa27dc3fc9f5bf
tanyuehong <tanyuehong@zuoyebang.com>
2022-03-24 15:15:12 +0800

    代码改动

--- service/service_redclass/src/test/java/com/redclass/code/CodeGenerator.java
+++ service/service_redclass/src/test/java/com/redclass/code/CodeGenerator.java
@@ -59,8 +59,8 @@ public class CodeGenerator {
 
         // 5、策略配置
         StrategyConfig strategy = new StrategyConfig();
-       // strategy.setInclude("red_qustion_good","red_blog_good");
-        strategy.setInclude("red_reply_comment_good");
+        strategy.setInclude("red_ask_waring","red_ask_advise");
+        //strategy.setInclude("red_reply_comment_good");
 
         strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
         strategy.setTablePrefix(pc.getModuleName() + "_"); //生成实体时去掉表前缀
