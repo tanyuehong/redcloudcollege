f05cb04652bef821e2427e0e6a097cd4d327da77
tanyuehong <tanyuehong@zuoyebang.com>
2022-03-29 14:03:07 +0800

    举报功能完善

--- service/service_redclass/src/main/java/com/redskt/classroom/entity/RedAskWaring.java
+++ service/service_redclass/src/main/java/com/redskt/classroom/entity/RedAskWaring.java
@@ -1,7 +1,10 @@
 package com.redskt.classroom.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
@@ -37,16 +40,18 @@ public class RedAskWaring implements Serializable {
 
     private String content;
 
-    @ApiModelProperty(value = "类型名字")
-    private String typename;
+    @ApiModelProperty(value = "举报类型")
+    private String jubaotype;
 
     @ApiModelProperty(value = "类型")
     private Integer type;
 
     @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
     private Date gmtCreate;
 
     @ApiModelProperty(value = "更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date gmtModified;
 
 
